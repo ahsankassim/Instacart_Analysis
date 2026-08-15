3a536dd86eaa8d689ef033e5afed6aa120ed95e1
Mohamed Ahsan <ahsankassim@gmail.com>
2026-08-15 18:48:04 +0530

    staging, model testing done

--- dbt/models/staging/stg_orders.sql
+++ dbt/models/staging/stg_orders.sql
@@ -1,14 +1,16 @@
 SELECT
-    order_id,
-    user_id,
-    order_number,
-    day_name AS order_day,
-    order_hour_of_day AS order_hour,
-    days_since_prior_order
+    O1.order_id,
+    O1.user_id,
+    O1.order_number,
+    O2.day_name AS order_day,
+    O1.order_hour_of_day AS order_hour,
+    O1.days_since_prior_order
 FROM
     {{ source('source', 'orders') }} O1
 LEFT JOIN
     {{ ref('order_dow_mapping') }} O2
 ON
     O1.order_dow = O2.order_dow
+WHERE
+    O1.eval_set != 'test'
 
