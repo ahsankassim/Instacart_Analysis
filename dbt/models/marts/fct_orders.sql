ccf2e9c8642a9aab24b199d795102c487428fb0a
code.ashif <ashiffeather@gmail.com>
2026-08-21 17:25:41 +0530

    commit for 21 aug, fct_orders with errors

---
+++ dbt/models/marts/fct_orders.sql
@@ -0,0 +1,24 @@
+WITH BASKET_SIZE AS (
+    
+    SELECT 
+        O.ORDER_ID, COUNT(O.ORDER_ID) AS BASKET_SIZE
+    FROM 
+        {{ ref('stg_orders')}} O
+    LEFT JOIN 
+        {{ ref('stg_order_products')}} OP
+    ON 
+        O.ORDER_ID = OP.ORDER_ID
+    GROUP BY 
+        O.ORDER_ID
+)
+
+
+
+SELECT 
+    O.*, BASKET_SIZE
+FROM 
+    {{ ref('stg_orders') }} O
+JOIN 
+    BASKET_SIZE B
+ON 
+    O.ORDER_ID = B.ORDER_ID
